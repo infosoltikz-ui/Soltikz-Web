c391e6098b71df567319f879617f700c48f3dc45
balajiprojects049 <balajiprojects049@gmail.com>
2026-07-21 12:50:21 +0530

    fix: move Vercel serverless function to project root

---
+++ api/index.js
@@ -0,0 +1,4 @@
+require('dotenv').config();
+const app = require('../backend/dist/app').default;
+
+module.exports = app;
--- backend/api/index.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config();
-const app = require('../dist/app').default;
-
-module.exports = app;
